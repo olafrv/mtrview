cd8fe93a55de177375a681bc727c303208eb6080
Olaf Reitmaier <olafrv@gmail.com>
2020-02-02 09:27:12 -0800

    Removing useless files

--- others/geoip.js
+++
@@ -1,6 +0,0 @@
-const Reader = require('@maxmind/geoip2-node').Reader;
-options = {};
-Reader.open('geoip.mmdb', options).then(reader => {
-    reader.asn("127.0.0.1").then(()=>{}).catch(()=>{});
-}).catch(()=>{});
-
