0f250fdccf5b9146f8bb83c084d84fa73e870970
Olaf Reitmaier <olafrv@gmail.com>
2020-02-09 17:42:48 -0800

    Multi Routes + Dates

--- ui/src/components/Graph.vue
+++ ui/src/components/Graph.vue
@@ -10,20 +10,21 @@ export default {
     fetchData() {
       var self = this;
       axios.get(
-          'http://192.168.2.225:8081?hostname=8.8.8.8',
+          'http://192.168.2.225:8081?hostname=www.google.com',
           { crossDomain: true }
         ).then((response) => {
           let nodes = [];
           let edges = [];
+          let n = 1;
           for(let hash in response.data.routes){
-            let n = 1;
-            for(let i in response.data.routes[hash].hops){
+              for(let i in response.data.routes[hash].hops){
               nodes.push(
                 { id : n , label : response.data.routes[hash].hops[i].ip }
               );
               edges.push({from: n, to: (n+1)});
               n++;
             }
+            n++;
           }
           self.nodes.update(
             //[{id: 1, label: 'Node 1'}, {id: 2, label: 'Node 2'}, {id: 3, label: 'Node 3'}, {id: 4, label: 'Node 4'}, {id: 5, label: 'Node 5'}]
--- ui/src/components/Routes.vue
+++ ui/src/components/Routes.vue
@@ -16,21 +16,22 @@
 
 <script>
   const axios = require('axios');
+  const dateFormat = require('dateformat');
   export default {
     methods : {
       fetchData(){
         var self = this;
         axios.get(
-            'http://192.168.2.225:8081?hostname=8.8.8.8',
+            'http://192.168.2.225:8081?hostname=www.google.com',
             { crossDomain: true }
           ).then(function (response) {
             let routes = [];
             let data = response.data.routes;
             for(let hash in data){
                 let route = {};
-                route.hash = data[hash].hash;
-                route.created = data[hash].created;
-                route.updated = data[hash].updated;
+                route.hash = data[hash].hash.substring(data[hash].hash.length - 6) ;
+                route.created = dateFormat(data[hash].created, "isoDateTime");
+                route.updated = dateFormat(data[hash].updated, "isoDateTime");
                 route.packets = data[hash].packets;
                 route.hops = data[hash].hopsUniq.join(' - ');
                 routes.push(route);
